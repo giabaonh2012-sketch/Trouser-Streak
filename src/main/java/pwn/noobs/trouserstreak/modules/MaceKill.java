9715d6b8a90b3936aacefe485d7211ef3099f8d6
giabaonh2012-sketch <giabaonh2012@gmail.com>
2026-06-04 18:08:49 +0700

    Update MaceKill.java

--- src/main/java/pwn/noobs/trouserstreak/modules/MaceKill.java
+++ src/main/java/pwn/noobs/trouserstreak/modules/MaceKill.java
@@ -41,9 +41,9 @@ public class MaceKill extends Module {
             .name("Fall height")
             .description("Simulates a fall from this distance")
             .defaultValue(22)
-            .sliderRange(1, 169)
+            .sliderRange(1, 400)
             .min(1)
-            .max(169)
+            .max(400)
             .build());
     private final Setting<Integer> paperpackets = specialGroup.add(new IntSetting.Builder()
             .name("# spam packets")
